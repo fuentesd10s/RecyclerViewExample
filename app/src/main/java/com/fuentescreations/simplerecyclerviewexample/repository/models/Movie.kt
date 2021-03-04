726667eb271fcb3be97e0ccc5d22ef0c3c70b439
Gonzalo <fuentesgonzalo@live.com>
2021-03-04 00:07:59 -0300

    Preparing to add Movie tab

--- app/src/main/java/com/fuentescreations/simplerecyclerviewexample/repository/models/Movie.kt
+++ app/src/main/java/com/fuentescreations/simplerecyclerviewexample/repository/models/Movie.kt
@@ -1,18 +1,28 @@
 package com.fuentescreations.simplerecyclerviewexample.repository.models
 
+import com.google.gson.annotations.SerializedName
+
 data class Movie (
         val id: Int = -1,
         val adult: Boolean = false,
+        @SerializedName("backdrop_path")
         val backdrop_path: String = "",
+        @SerializedName("original_title")
         val original_title: String = "",
+        @SerializedName("original_language")
         val original_language: String = "",
         val overview: String = "",
         val popularity: Double = -1.0,
-        val poster_path: String = "",
-        val release_date: String = "",
+        @SerializedName("poster_path")
+        val posterPath: String = "",
+        @SerializedName("release_date")
+        val releaseDate: String = "",
         val title: String = "",
         val video: Boolean = false,
-        val vote_average: Double = -1.0,
-        val vote_count: Int = -1,
-        val movie_type: String = ""
+        @SerializedName("vote_average")
+        val voteAverage: Double = -1.0,
+        @SerializedName("voteCount")
+        val voteCount: Int = -1,
+        @SerializedName("movie_type")
+        val movieType: String = ""
         )
--- app/src/main/java/com/fuentescreations/simplerecyclerviewexample/repository/models/Photos.kt
+++ app/src/main/java/com/fuentescreations/simplerecyclerviewexample/repository/models/Photos.kt
@@ -3,9 +3,10 @@ package com.fuentescreations.simplerecyclerviewexample.repository.models
 import com.google.gson.annotations.SerializedName
 
 data class Photos(
-    @SerializedName("id") val id:Int=-1,
-    @SerializedName("albumId") val albumId:Int=-1,
-    @SerializedName("title") val title:String="",
-    @SerializedName("url") val imageUrl:String="",
-    @SerializedName("thumbnailUrl") val thumbnailUrl:String=""
+    val id:Int=-1,
+    val albumId:Int=-1,
+     val title:String="",
+    @SerializedName("url")
+    val imageUrl:String="",
+    val thumbnailUrl:String=""
 )
